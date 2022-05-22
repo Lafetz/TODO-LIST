9642293a513d46ba945ba42de1905bce2cda2ebe
cosmic <yafetyohaness@gmail.com>
2022-05-22 15:41:26 +0300

    make functions to edit tasks

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
-import { newList, newTask } from "./functions";
-const tasks = (function () {
-  let lists = [];
-  const addList = function (name) {
-    let list = newList(name);
-    lists.push(list);
-  };
-  const removeList = function (listIndex) {
-    lists.splice(listIndex, 1);
-  };
-  const addTask = function (listI, title, description, priority) {
-    let task = newTask(title, description, priority);
-    lists[listI].tasks.push(task);
-  };
-  const removeTask = function (listIndex, taskIndex) {
-    lists[listIndex].tasks.splice(taskIndex, 1);
-  };
-  return {
-    addList: addList,
-    removeList: removeList,
-    addTask: addTask,
-    removeTask: removeTask,
-    lists,
-  };
-})();
+import { task } from "./task.js";
+const tasks = task();
 tasks.addList("List0");
 tasks.addList("List1");
 tasks.addList("List2");
@@ -30,10 +7,10 @@ tasks.addList("List3");
 tasks.addList("List4");
 tasks.addList("List5");
 tasks.addList("List6");
-tasks.addTask(1, "0", "djdjjdj", "jdjdj");
-tasks.addTask(1, "1", "djdjjdj", "jdjdj");
-tasks.addTask(1, "2", "gone", "jdjdj");
-tasks.addTask(1, "3", "djdjjdj", "jdjdj");
-tasks.addTask(1, "4", "djdjjdj", "jdjdj");
-console.log(tasks.lists);
-tasks.removeTask(1, 2);
+tasks.addTask(1, "get dinner ready", "order pizza", 1);
+tasks.addTask(1, "feed birds", "give it to them before noon", 2);
+tasks.addTask(1, "do this", "through this", 2);
+tasks.addTask(1, "sleep", "before 6", 3);
+tasks.addTask(1, "go to gym", "return the book on the way", 1);
+
+console.log(tasks.lists[1]);
---
+++ src/task.js
@@ -0,0 +1,42 @@
+import { newList, newTask } from "./functions";
+const task = function () {
+  let lists = [];
+  const addList = function (name) {
+    let list = newList(name);
+    lists.push(list);
+  };
+  const removeList = function (listIndex) {
+    lists.splice(listIndex, 1);
+  };
+  const editList = function (listIndex, newName) {
+    lists[listIndex].name = newName;
+  };
+  const addTask = function (listI, title, description, priority) {
+    let task = newTask(title, description, priority);
+    lists[listI].tasks.push(task);
+  };
+  const removeTask = function (listIndex, taskIndex) {
+    lists[listIndex].tasks.splice(taskIndex, 1);
+  };
+  const editTaskTitle = function (listIndex, taskIndex, newtitle) {
+    lists[listIndex].tasks[taskIndex].title = newtitle;
+  };
+  const editTaskDesc = function (listIndex, taskIndex, newDescription) {
+    lists[listIndex].tasks[taskIndex].description = newDescription;
+  };
+  const editpriority = function (listIndex, taskIndex, newpriority) {
+    lists[listIndex].tasks[taskIndex].priority = newpriority;
+  };
+  return {
+    addList: addList,
+    removeList: removeList,
+    addTask: addTask,
+    removeTask: removeTask,
+    editList: editList,
+    editTaskTitle: editTaskTitle,
+    editTaskDesc: editTaskDesc,
+    editpriority: editpriority,
+    lists,
+  };
+};
+export { task };
